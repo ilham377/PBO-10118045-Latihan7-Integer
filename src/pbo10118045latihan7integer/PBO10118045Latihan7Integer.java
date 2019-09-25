ea9f80a2ebd2ade2d12cd77719e1b7a52053c436
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 20:50:44 +0700

    first commit

---
+++ src/pbo10118045latihan7integer/PBO10118045Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan7integer;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : integer
+ */
+public class PBO10118045Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 9000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : "+ i);
+        System.out.println("long : " +l);
+    }
+    
+}
